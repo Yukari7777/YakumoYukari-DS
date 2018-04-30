ad4c488e7ca8617d81cb8f6ccd2b5e360f2aae88
YakumoYukari <yys14239102@gmail.com>
2018-04-30 18:03:29 +0900

    modinfo
    10.0.14 업데이트 최종

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Yakumo Yukari"
 description = "Yakumo Yukari comes from unknown world to manipulate Don't Starve world!"
 author = "Yakumo Yukari"
-version = "0.10.0.13"
+version = "0.10.0.14"
 forumthread = ""
 api_version = 6
 
