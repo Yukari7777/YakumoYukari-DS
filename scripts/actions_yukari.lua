1ee18ac8fa7e9b7dbdc5993b8aebd5106fa9ad20
HimekaidouHatate <HimekaidouHatate@users.noreply.github.com>
2019-02-26 19:56:10 +0900

    0226

--- scripts/actions_yukari.lua
+++ scripts/actions_yukari.lua
@@ -1,43 +1,39 @@
 local STRINGS = GLOBAL.STRINGS
 local EQUIPSLOTS = GLOBAL.EQUIPSLOTS
-local ACTIONS = GLOBAL.ACTIONS
+local Action = GLOBAL.Action
 
-----------------------------------------------------------------------------------------------------
-
-local YTELE = AddAction("YTELE", "Teleport", function(act)
+local YTELE = Action({}, 10, nil, true, 14)
+YTELE.id = "YTELE"
+YTELE.str = STRINGS.ACTION_YTELE
+YTELE.fn = function(act)
 	if act.invobject and act.invobject.components.makegate then
 		return act.invobject.components.makegate:Teleport(act.pos, act.doer)
 	end
-end)
-YTELE.priority = 10
-YTELE.distance = 14
-YTELE.rmb = true
-YTELE.mount_valid = false
+end
+AddAction(YTELE)
 
----------------------------------------------------------------------------------------------------
-
-local CASTTOHO = AddAction("CASTTOHO", "castspell", function(act)
+local CASTTOHO = Action({}, -1, nil, true)
+CASTTOHO.id = "CASTTOHO"
+CASTTOHO.str = STRINGS.ACTION_CASTTOHO
+CASTTOHO.fn = function(act)
 	local item = act.invobject or act.doer.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS)
 
 	if item and item.components.spellcard ~= nil then
 		item.components.spellcard:CastSpell(act.doer, act.target)
 		return true
 	end
-end)
-CASTTOHO.priority = -1
-CASTTOHO.rmb = true
-CASTTOHO.mount_valid = false
+end
+AddAction(CASTTOHO)
 
----------------------------------------------------------------------------------------------------
-
-local CASTTOHOH = AddAction("CASTTOHOH", "castspell", function(act)
+local CASTTOHOH = Action({}, -1, nil, true)
+CASTTOHOH.id = "CASTTOHOH"
+CASTTOHOH.str = STRINGS.ACTION_CASTTOHOH
+CASTTOHOH.fn = function(act)
 	local item = act.invobject or act.doer.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS)
 
-	if item and item.components.spellcard then
+	if item and item.components.spellcard ~= nil then
 		item.components.spellcard:CastSpell(act.doer, act.target)
 		return true
 	end
-end)
-CASTTOHOH.priority = -1
-CASTTOHOH.rmb = true
-CASTTOHOH.mount_valid = false
+end
+AddAction(CASTTOHOH)
